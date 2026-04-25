731605263bd16e1d650b793cbcd8a1de0f7df4b0
akshaypandey28 <akshaychaman2004@gmail.com>
2026-04-25 21:00:08 +0530

    added repository layer for city

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 
-const {PORT} = require('./config/serverConfig.js')
+const {City} = require('./models/index.js');
+const db = require('./models/index.js'); //it will fetch the db object
+const {PORT} = require('./config/serverConfig.js');
 
 const setUpAndStartServer = async () =>{
 
@@ -9,8 +11,11 @@ const setUpAndStartServer = async () =>{
     app.use(express.json());
     app.use(express.urlencoded({extended:true}));
 
-    app.listen(PORT, () => {
+    app.listen(PORT, async () => {
         console.log(`Server started at port ${PORT}`);
+        await City.create({ //this is promise that's why await
+            name: "New Delhi",
+        })
     })
 }
 
---
+++ src/repository/city-repository.js
@@ -0,0 +1,2 @@
+const City = require('../models/index.js'); //it will return all the corresponding model
+
