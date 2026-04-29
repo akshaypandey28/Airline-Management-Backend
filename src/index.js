8c2dfdc95748108e47a519f06ec8ff361b5aea4b
akshaypandey28 <akshaychaman2004@gmail.com>
2026-04-30 00:16:23 +0530

    added router to create api

--- src/index.js
+++ src/index.js
@@ -1,14 +1,18 @@
 const express = require('express');
 
 const {PORT} = require('./config/serverConfig.js');
-const CityRepository = require('./repository/city-repository.js');
+
+const ApiRoutes = require('./routes/index.js'); //ApiRoutes is a router
 
 const setUpAndStartServer = async () =>{
 
     //create the express object
     const app = express();
+
     app.use(express.json());
     app.use(express.urlencoded({extended:true}));
+
+    app.use('/api',ApiRoutes); 
 
     app.listen(PORT, async () => {
         console.log(`Server started at port ${PORT}`);
