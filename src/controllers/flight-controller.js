00a1c24a0c6fcb1bc9d3a928b6226a93ae0bcfeb
akshaypandey28 <akshaychaman2004@gmail.com>
2026-05-02 01:50:41 +0530

    added flight-create request Middleware

--- src/controllers/flight-controller.js
+++ src/controllers/flight-controller.js
@@ -1,11 +1,21 @@
 const {FlightService} = require('../services/index.js');
+const { SuccessCodes } = require('../utils/error-codes');
 
 const flightService = new FlightService();
 
 const create = async(req,res) => {
     try {
-        const flight = await flightService.createFlight(req.body);
-        res.status(200).json({
+        const flightRequestData = {
+            flightNumber: req.body.flightNumber,
+            airplaneId: req.body.airplaneId,
+            departureAirportId: req.body.departureAirportId,
+            arrivalAirportId: req.body.arrivalAirportId,
+            arrivalTime: req.body.arrivalTime,
+            departureTime: req.body.departureTime,
+            price: req.body.price
+        }
+        const flight = await flightService.createFlight(flightRequestData);
+        res.status(SuccessCodes.CREATED).json({
             data:flight,
             success:true,
             message: 'Successfully created a flight',
@@ -47,7 +57,7 @@ const getAll = async (req, res) => {
     try {
         console.log("DATA OF QUERY PARAMS ------------- ",req.query);
         const response = await flightService.getAllFlightData(req.query);
-        return res.status(200).json({
+        return res.status(SuccessCodes.OK).json({
             data: response,
             success: true,
             err: {},
--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -1,4 +1,6 @@
 const express = require('express');
+
+const { FlightMiddlewares } = require('../../middlewares/index.js');
 const CityController = require('../../controllers/city-controller.js');
 const FlightController = require('../../controllers/flight-controller.js');
 const AirportController = require('../../controllers/airport-controller.js');
@@ -13,8 +15,14 @@ router.patch('/city/:id', CityController.update);
 router.get('/city', CityController.getAll);
 
 //for flights
-router.post('/flights',FlightController.create);
+router.post(
+    '/flights', 
+    FlightMiddlewares.validateCreateFlight, 
+    FlightController.create
+);
 router.get('/flights',FlightController.getAll);
+router.get('/flights/:id', FlightController.get);
+//router.patch('/flights/:id', FlightController.update);
 
 //for airports
 router.post('/airports',AirportController.create);
