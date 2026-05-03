c6084ff245509c5d9eb203a6eab2c839630d50c5
akshaypandey28 <akshaychaman2004@gmail.com>
2026-05-03 12:44:40 +0530

    added JWT

--- src/services/user-services.js
+++ src/services/user-services.js
@@ -1,4 +1,7 @@
+const jwt = require('jsonwebtoken');
+
 const UserRepository = require('../repository/user-repository.js');
+const {JWT_KEY} = require('../config/serverConfig.js');
 
 class UserService {
     constructor(){
@@ -15,7 +18,24 @@ class UserService {
         }
     }
 
+    createToken(user){ //user is object
+        try {
+            const result = jwt.sign(user, JWT_KEY, {expiresIn: '1d'});
+            return result;
+        } catch (error) {
+            console.log("Something went wrong in the service layer while token creation");
+        }
+    }
 
+    verifyToken(token) {
+        try {
+            const response = jwt.verify(token, JWT_KEY); //returns the payload (data) that was used when the token was created, after verifying it
+            return response;
+        } catch (error) {
+            console.log("Something went wrong in the service layer while token validation", error);
+            throw error;
+        }
+    }
 }
 
 module.exports = UserService;
