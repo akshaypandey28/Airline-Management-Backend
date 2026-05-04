4973d2ea41bced42314ca0f4f498eaa386e2d8ef
akshaypandey28 <akshaychaman2004@gmail.com>
2026-05-04 23:19:00 +0530

    setUp basic setup

---
+++ src/index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+
+const app = express();
+const { PORT } = require('./config/serverConfig.js');
+
+const apiRoutes = require('./routes/index.js');
+
+const db = require('./models/index.js');
+
+const setUpAndStartServer = () => {
+
+    app.use(express.json());
+
+    app.use(express.urlencoded({ extended: true }));
+    
+    app.use('/api', apiRoutes);
+    app.listen(PORT, () => {
+        console.log(`Server is running on port ${process.env.PORT}`);
+
+        if(process.env.DB_SYNC) {
+            db.sequelize.sync({ alter: true });
+        }
+    });
+};
+
+setUpAndStartServer();
